592cc2f9815a6f6ad2b2d72419adc14844028e84
Gergely Ferenczy <gergo.ferenczy1@gmail.com>
2023-03-01 11:10:56 +0100

    UI backend communication works.

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,116 @@
-import React from 'react'
-import * as THREE from 'three';
+import deepEqual from 'deep-equal'
+import * as THREE from 'three'
+import React, { useState, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Grid, OrbitControls, GizmoHelper, GizmoViewport, Plane } from '@react-three/drei'
+import { Grid, OrbitControls, GizmoHelper, GizmoViewcube, Plane } from '@react-three/drei'
 
+import './App.css'
 import BodyElement from './BodyElement'
 
-const envSize = { x: 0.4, y: 0.4, z: 0.4 }
+const ENV_SIZE = { x: 0.4, y: 0.4, z: 0.4 }
 
 
-const hexaData = [
-  [ [ 0.07,   0.03,  -0.04], [0.1, 0.078, -0.07], [0.14, 0.014, -0.11] ],
-  [ [ 0.0,    0.03,  -0.05], [], [] ],
-  [ [-0.07,   0.03,  -0.04], [], [] ],
-  [ [ 0.07,   0.03,   0.04], [], [] ],
-  [ [ 0.0,    0.03,   0.05], [], [] ],
-  [ [-0.07,   0.03,   0.04], [], [] ]
-];
+const ControlPacket = {
+  speed: 0.2,
+  step: [0.04, 0.0, 0.0],
+  step_height: 0.3,
+  move: false
+};
+const BackendChannel = new WebSocket("ws://localhost:8080");
 
-console.log(new THREE.Vector3(0, -0.08, 0).applyAxisAngle(
-            new THREE.Vector3(1, 0, 1), Math.PI / 6).add(
-            new THREE.Vector3(0.1, 0.078, -0.07)));
+
 
 
 const App = () => {
+  const [posData, _setPosData] = useState();
+  const posDataRef = React.useRef(posData);
+  const setPosData = data => {
+    posDataRef.current = data;
+    _setPosData(data);
+  };
+
+  useEffect(() => {
+    BackendChannel.onmessage = (event) => {
+      const newPosData = JSON.parse(event.data);
+      if (!deepEqual(posDataRef.current, newPosData)) {
+        setPosData(newPosData);
+      }
+    }
+    return () => { BackendChannel.onmessage == null; };
+  }, []);
+
+
+  let minLegHeightPos;
+  let bodyPlaneNormal;
+  let bodyPlaneRotation;
+  const minLegPositions = [];
+
+  if (posData) {    
+    minLegPositions[1] = new THREE.Vector3(posData.legs[0][2][0], posData.legs[0][2][1], posData.legs[0][2][2]);
+    minLegPositions[2] = new THREE.Vector3(posData.legs[0][2][0], posData.legs[0][2][1], posData.legs[0][2][2]);
+    
+    for (var i = 0; i < 6; i++) {
+      minLegPositions[i] = new THREE.Vector3(posData.legs[i][2][0], posData.legs[i][2][1], posData.legs[i][2][2]);
+    }
+    minLegPositions.sort((a,b) => { return a.y - b.y });
+
+    const vec1 = minLegPositions[1].clone().sub(minLegPositions[0]);
+    const vec2 = minLegPositions[2].clone().sub(minLegPositions[0]);
+    bodyPlaneNormal = vec2.clone().cross(vec1).normalize();
+    bodyPlaneRotation = new THREE.Quaternion().identity();// setFromUnitVectors(bodyPlaneNormal, THREE.Object3D.DefaultUp);
+    minLegHeightPos = 0;//minLegPositions[0].applyQuaternion(bodyPlaneRotation).y;
+  }
+
   return (
     <div id="canvas-container">
-      <Canvas shadows
+      <Canvas shadows frameloop="demand"  rotation={[0, 0, 0]}
         style={{width: window.innerWidth, height: window.innerHeight}}
-        camera={{ fov: 25, near: 0.01, far: 1000, position: [-envSize.x, envSize.y, -envSize.z] }}>
+        camera={{ fov: 25, near: 0.01, far: 1000, position: [-ENV_SIZE.x, ENV_SIZE.y, -ENV_SIZE.z] }}>
 
-        <ambientLight intensity={0.3} />
-        <directionalLight color='white' intensity={1} position={[0, 1, 0]} 
+        <ambientLight intensity={0.5} />
+        <directionalLight color='white' intensity={1} 
           castShadow
           shadow-mapSize-height={1024} 
           shadow-mapSize-width={1024}>
-            <orthographicCamera attach="shadow-camera" args={[-envSize.x/2, envSize.x/2, envSize.z/2, -envSize.z/2]} />
+            <orthographicCamera attach="shadow-camera" args={[-ENV_SIZE.x/2, ENV_SIZE.x/2, ENV_SIZE.y/2, -ENV_SIZE.y/2]} />
         </directionalLight>
 
-        <Grid receiveShadow position={[0, 0, 0]} args={[envSize.x, envSize.z]} cellSize={0.02} cellThickness={0.6} cellColor={'#6f6f6f'} 
+        <Grid receiveShadow args={[ENV_SIZE.x, ENV_SIZE.z]} cellSize={0.02} cellThickness={0.6} cellColor={'#6f6f6f'} 
           sectionSize={0.1} sectionThickness={1.5} sectionColor={'#9d4b4b'} fadeStrength={0} />
 
-        <Plane receiveShadow rotation={[-Math.PI/2, 0, 0]} position={[0, -0.001, 0]} args={[envSize.x, envSize.z]} >
-          <shadowMaterial attach="material" />
+        <Plane receiveShadow rotation={[-Math.PI/2, 0, 0]} position={[0, -0.001, 0]} args={[ENV_SIZE.x, ENV_SIZE.z]} >
+          <meshStandardMaterial attach="material" color="#303035" transparent opacity={0.7} />
         </Plane>
-        <OrbitControls /*autoRotate={true} autoRotateSpeed={0.5}*/ enableDamping={false} makeDefault />
-        <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
-          <GizmoViewport axisColors={['#9d4b4b', '#2f7f4f', '#3b5b9d']} labelColor="white" />
+        <OrbitControls enableDamping={false} makeDefault />
+        <GizmoHelper alignment="bottom-right" margin={[80, 80]} >
+          <GizmoViewcube />
         </GizmoHelper>
 
-        <BodyElement start={ hexaData[0][0] } end={hexaData[1][0]} />
-        <BodyElement start={ hexaData[1][0] } end={hexaData[2][0]} />
-        <BodyElement start={ hexaData[2][0] } end={hexaData[5][0]} />
-        <BodyElement start={ hexaData[5][0] } end={hexaData[4][0]} />
-        <BodyElement start={ hexaData[4][0] } end={hexaData[3][0]} />
-        <BodyElement start={ hexaData[3][0] } end={hexaData[0][0]} />
-        <BodyElement start={ hexaData[0][0] } end={hexaData[5][0]} />
-        <BodyElement start={ hexaData[2][0] } end={hexaData[3][0]} />
+        {posData &&
+          <group quaternion={bodyPlaneRotation} position={[0, -minLegHeightPos, 0]} >
+            <BodyElement start={ posData.legs[0][0] } end={posData.legs[1][0]} />
+            <BodyElement start={ posData.legs[1][0] } end={posData.legs[2][0]} />
+            <BodyElement start={ posData.legs[2][0] } end={posData.legs[5][0]} />
+            <BodyElement start={ posData.legs[5][0] } end={posData.legs[4][0]} />
+            <BodyElement start={ posData.legs[4][0] } end={posData.legs[3][0]} />
+            <BodyElement start={ posData.legs[3][0] } end={posData.legs[0][0]}  color="red" />
+            <BodyElement start={ posData.legs[0][0] } end={posData.legs[5][0]} />
+            <BodyElement start={ posData.legs[2][0] } end={posData.legs[3][0]} />
 
-        <BodyElement start={ hexaData[0][0] } end={hexaData[0][1]} startBump />
-        <BodyElement start={ hexaData[0][1] } end={hexaData[0][2]} startBump />
+            <BodyElement start={ posData.legs[0][0] } end={posData.legs[0][1]} startBump />
+            <BodyElement start={ posData.legs[0][1] } end={posData.legs[0][2]} startBump endBump />
+            <BodyElement start={ posData.legs[1][0] } end={posData.legs[1][1]} startBump />
+            <BodyElement start={ posData.legs[1][1] } end={posData.legs[1][2]} startBump endBump />
+            <BodyElement start={ posData.legs[2][0] } end={posData.legs[2][1]} startBump />
+            <BodyElement start={ posData.legs[2][1] } end={posData.legs[2][2]} startBump endBump />
+            <BodyElement start={ posData.legs[3][0] } end={posData.legs[3][1]} startBump />
+            <BodyElement start={ posData.legs[3][1] } end={posData.legs[3][2]} startBump endBump />
+            <BodyElement start={ posData.legs[4][0] } end={posData.legs[4][1]} startBump />
+            <BodyElement start={ posData.legs[4][1] } end={posData.legs[4][2]} startBump endBump />
+            <BodyElement start={ posData.legs[5][0] } end={posData.legs[5][1]} startBump />
+            <BodyElement start={ posData.legs[5][1] } end={posData.legs[5][2]} startBump endBump />
+          </group>
+        }
 
       </Canvas>
     </div>
--- src/BodyElement.jsx
+++ src/BodyElement.jsx
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 
 const BodyElement = (props) => {
 
-  const verticalVec = new THREE.Vector3(0,1,0);
+  const verticalVec = THREE.Object3D.DefaultUp;
   const startVec = new THREE.Vector3(props.start[0], props.start[1], props.start[2]);
   const endVec = new THREE.Vector3(props.end[0], props.end[1], props.end[2]);
   const legVec = endVec.clone().sub(startVec);
@@ -16,18 +16,20 @@ const BodyElement = (props) => {
   const quaternion = new THREE.Quaternion();
   quaternion.setFromAxisAngle(normalVec, angle);
 
+  const material = <meshStandardMaterial color={props.color || '#008080'} />;
+
   let startBump;
   if (props.startBump) {
     startBump = <mesh castShadow position={startVec} >
         <sphereGeometry args={[width * 2]} />
-        <meshStandardMaterial color='#008080' />
+        {material}
       </mesh>;
   }
   let endBump;
   if (props.endBump) {
     endBump = <mesh castShadow position={endVec} >
         <sphereGeometry args={[width * 2]} />
-        <meshStandardMaterial color='#008080' />
+        {material}
       </mesh>;
   }
 
@@ -35,7 +37,7 @@ const BodyElement = (props) => {
     <group>
       <mesh castShadow position={middleVec} quaternion={quaternion} >
         <cylinderGeometry args={[width, width, length]} />
-        <meshStandardMaterial color='#008080' />
+        {material}
       </mesh>
       {startBump}
       {endBump}
